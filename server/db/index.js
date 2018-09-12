2c3cb87883984105c5b03fec3a703f617fea5e6b
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-11 21:07:08 -0700

    [WIP] -setting up for Sequalize refactor

--- server/db/index.js
+++ server/db/index.js
@@ -1,34 +1,14 @@
-var mysql = require('mysql');
+var Sequelize = require('sequelize');
+var db = new Sequelize('chat', 'root', '');
 
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
-
-dbConnection = mysql.createConnection({
-  user: 'root',
-  password: '',
-  database: 'chat'
+var User = db.define('User', {
+  username: Sequelize.STRING
 });
-dbConnection.connect();
 
-// dbConnection.query('INSERT INTO messages(userid, text, roomname) VALUES("444", "Hey I the first text", "newRoom")', 
-//   (err, results, fields) => {
-//     if (err) {
-//       console.log('I am an error: ', err);
-//       throw err;
-//     }
-//     console.log('Record inserted successfully');
-//     console.log('These are the results: ', results);
-//   });
-
-// dbConnection.query('SELECT * FROM messages', 
-//   (err, results, fields) => {
-//     if (err) {
-//       console.log('I am an error: ', err);
-//       throw err;
-//     }
-//     console.log('These are the results: ', results);
-//   });
+var Message = db.define('Message', {
+  userid: Sequelize.INTEGER,
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
 
 module.exports = dbConnection;
